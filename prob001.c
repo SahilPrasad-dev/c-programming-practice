ff08ee886f15a12df646267efb68089b358ecd11
SahilPrasad-dev <sahil.prasad.dev@gmail.com>
2026-09-14 10:33:05 +0530

    Displaying the student details

--- prob001.c
+++ prob001.c
@@ -1,12 +1,16 @@
-//STUDENT ENROLMENT DETAILS
+// Student Enrolment Details  
+// You are a newly admitted student in MLRIT.
+// During registration, the computer system prints your name, your branch of study and College Name.
+// So, your task is to print your information with college name 
+// neatly—just like the administration software would do for every student.
 
 #include <stdio.h>
 
 int main()
 {
-	printf("College: MLR Institute of Technology\n");
-	printf("Name: Sahil Kumar Prasad\n");
-	printf("Branch: CSE\n");
+	printf("College : MLR Institute of Technology\n");
+	printf("Name    : Sahil Kumar Prasad\n");
+	printf("Branch  : CSE\n");
 
 	return 0;
 }
